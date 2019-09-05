d334b6bdfbdcacf1acccf0d7de8e568e8b7fa2e2
Start_Hammond <53830640+Hammond-sys@users.noreply.github.com>
2019-09-05 20:01:15 +0700

    Add files via upload

---
+++ bahan.sh
@@ -0,0 +1,16 @@
+clear
+echo "wellcome to my hacking"|lolcat
+echo ""
+clear
+echo 'sedang menginstall bahan'|lolcat
+echo ''
+echo ''
+pkg install git python python2 ruby && gem install lolcat
+clear
+git clone https://github.com/Hammond-sys/terkey
+cd terkey
+python2 terkey.py
+cd $home
+pip2 install requests mechanize
+echo "selesai bang"|lolcat
+login
